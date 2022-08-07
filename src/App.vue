c09cbd1523be17abaa1c895462eba2bd6fb611db
Kevan Williams <kevwilliams012@gmail.com>
2022-08-07 10:41:40 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="relative bg-black h-full min-h-screen">
+    <TheNavBar />
+    <div class="pt-10 px-10 flex justify-center align-middle">
+      <p class="mt-10 text-4xl pt-3 px-1 mt-1 text-white">STAKE YOUR NFT</p>
+
+
+
+    </div>
+    <div class="slider">
+      <div class="slide-track">
+        <div class="slide">
+          <img class="sli" src="./assets/1.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/2.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/3.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/4.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/5.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/6.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/7.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/8.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/9.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/1.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/2.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/3.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/4.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/5.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/6.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/7.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/8.png" />
+        </div>
+        <div class="slide">
+          <img class="sli" src="./assets/9.png" />
+        </div>
+
+      </div>
+
+    </div>
+
+
+    <div class="p-10">
+      <router-view />
+    </div>
+
+    <div class="pt-10"></div>
+
+
+  </div>
+</template>
+
+<script>
+import TheNavBar from '@/components/TheNavBar';
+
+
+export default {
+  components: { TheNavBar },
+};
+</script>
+
+<style>
+* {
+  font-family: 'Mochiy Pop One', sans-serif;
+}
+
+input[type='radio']:checked+span {
+  @apply text-black;
+}
+</style>
---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="py-6 bg-black flex flex-row justify-center border-b border-gray-200 align-middle">
+    <NavButton v-for="i in menu" :key="i.name" :title="i.name" :url="i.url" :class="{ active: path === i.url }" />
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import NavButton from '@/components/NavButton.vue';
+
+export default defineComponent({
+  components: { NavButton },
+  setup() {
+    const route = useRoute();
+    const path = computed(() => route.path);
+
+    const menu = [
+      { name: 'Home', url: '/' },
+      { name: 'Mint', url: '/' },
+      { name: 'Team', url: '/' },
+      { name: 'FAQ', url: '/' },
+      //{ name: 'FARM MANAGER', url: '/manager' },
+      { name: 'Stake', url: '/farmer' },
+    ];
+
+    return {
+      menu,
+      path,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
